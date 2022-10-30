8b452637c7710bea3286b4227e836ee12234e6ef
tmmfo1 <tmmfo1@iscte-iul.pt>
2022-10-30 18:12:33 +0000

    3º teste

--- Projeto_ES/src/main/java/ES_2022_LETI_Grupo_15/Projeto_ES/App.java
+++ Projeto_ES/src/main/java/ES_2022_LETI_Grupo_15/Projeto_ES/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Projeto ES.velhote " );
+        System.out.println( "Projeto ES.filhote " );
     }
 }
