a044f56d8b19815643a3233cadea94b2baa8c6be
jrpre1-iscte <jrpre1@iscte-iul.pt>
2022-10-31 17:05:16 +0000

    Ler URL de fénix de membro e transformar em ficheiro texto.

---
+++ Projeto_ES/src/main/java/ES_2022_LETI_Grupo_15/Projeto_ES/ReadTextFromURL.java
@@ -0,0 +1,56 @@
+package ES_2022_LETI_Grupo_15.Projeto_ES;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.io.File;
+import java.io.FileWriter;
+
+public class ReadTextFromURL {
+
+	public static void main(String[] args) {
+
+		try {
+
+			URL url = new URL("https://fenix.iscte-iul.pt/publico/publicPersonICalendar.do?method=iCalendar&username=tmmfo1@iscte.pt&password=oN93nFF9kYTy0A5nUzsLh3wWkwPPkxJeZlYWXHbqGQNgQq2r57pTpOPlpPzcx1gjfgjFReV1Wp3OjNe1SdVSg8NuGDUy06UQ17URCUFVbRAeucNLayOdmeTHk3wEn96g\r\n"
+					+ "\r\n"
+					+ "");
+
+			try {
+				File myObj = new File("file.txt");
+				if (myObj.createNewFile()) {
+					System.out.println("File created: " + myObj.getName());
+				} else {
+					System.out.println("File already exists.");
+				}
+			} catch (IOException e) {
+				System.out.println("An error occurred.");
+				e.printStackTrace();
+			}
+
+
+			BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+
+			String line;
+			FileWriter myWriter = new FileWriter("file.txt");
+			while ((line = in.readLine()) != null) {
+				try {
+					myWriter.write(line);
+				} catch (IOException e) {
+					System.out.println("An error occurred.");
+					e.printStackTrace();
+				}
+			}
+			in.close();
+			myWriter.close();
+		}
+		catch (MalformedURLException e) {
+			System.out.println("Malformed URL: " + e.getMessage());
+		}
+		catch (IOException e) {
+			System.out.println("I/O Error: " + e.getMessage());
+		}
+	}
+}
