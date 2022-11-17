55fc6be0e458913a978392d27d1775aac597af77
jrpre1-iscte <jrpre1@iscte-iul.pt>
2022-11-17 15:42:18 +0000

    Menu: names and number of members

--- Projeto_ES/src/main/java/ES_2022_LETI_Grupo_15/Projeto_ES/GUI.java
+++ Projeto_ES/src/main/java/ES_2022_LETI_Grupo_15/Projeto_ES/GUI.java
@@ -10,17 +10,25 @@ import java.io.IOException;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JTextField;
 
 
 
 public class GUI extends JFrame implements ActionListener {
 
+	int count = 0;
+
 	JButton submit;
 	JButton file;
 	JTextField textField;
+	JTextField nome;
+	JTextField apelido;
+	JComboBox<Integer> numeroMembros = new JComboBox<Integer>();
+	JLabel label;
 
 	GUI(){		
 
@@ -36,6 +44,20 @@ public class GUI extends JFrame implements ActionListener {
 		this.pack();
 		this.setVisible(true);
 
+		nome = new JTextField();
+		nome.setPreferredSize(new Dimension(200,40));
+		nome.setFont(new Font("Arial", Font.PLAIN, 35));
+		this.add(nome);
+		this.pack();
+		this.setVisible(true);
+
+		apelido = new JTextField();
+		apelido.setPreferredSize(new Dimension(200,40));
+		apelido.setFont(new Font("Arial", Font.PLAIN, 35));
+		this.add(apelido);
+		this.pack();
+		this.setVisible(true);
+
 		submit = new JButton("Submit");
 		submit.setBounds(200, 100, 100, 50);
 		submit.setSize(200, 200);
@@ -43,12 +65,28 @@ public class GUI extends JFrame implements ActionListener {
 		this.setVisible(true);
 		this.add(submit);
 
+		label = new JLabel("Número de membros: 0");
+		this.add(label);
+
 		file = new JButton("Select File");
 		file.setSize(200, 200);
 		file.setLocation(800, 400);
 		file.addActionListener(this);
 		this.setVisible(true);
 		this.add(file);
+
+		numeroMembros.setSize(200, 200);
+		numeroMembros.setLocation(800, 400);
+		numeroMembros.addActionListener(this);
+		this.setVisible(true);
+		this.add(numeroMembros);
+
+		numeroMembros.addItem(1);
+		numeroMembros.addItem(2);
+		numeroMembros.addItem(3);
+		numeroMembros.addItem(4);
+		numeroMembros.addItem(5);
+		numeroMembros.addItem(6);
 
 		this.setTitle("Meeting planner");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -62,23 +100,32 @@ public class GUI extends JFrame implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 
 		if(e.getSource() == submit) {
+			if(count <= numeroMembros.getSelectedIndex()) {
 				try {
-					toTxt.convert(textField.getText(), "C:\\Users\\Asus\\Desktop\\testeICS.txt");
+					toTxt.convert(textField.getText(), "C:\\Users\\Lobato\\Desktop\\"+nome.getText()+apelido.getText()+".txt");
+					count++;
+					label.setText("Number of clicks: " + count);
+
+					textField.setText("");
+					nome.setText("");
+					apelido.setText("");
+
 				} catch (IOException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
 			}
 
-		if(e.getSource() == file) {
-            JFileChooser fileChooser = new JFileChooser();
-            fileChooser.showOpenDialog(null);
-            try {
-                toTxt.convert(fileChooser.getSelectedFile(), "C:\\Users\\Asus\\Desktop\\testeICS.txt");
-            } catch (IOException e1) {
-                // TODO Auto-generated catch block
-                e1.printStackTrace();
-            }
-        }
+			if(e.getSource() == file) {
+				JFileChooser fileChooser = new JFileChooser();
+				fileChooser.showOpenDialog(null);
+				try {
+					toTxt.convert(fileChooser.getSelectedFile(), "C:\\Users\\Asus\\Desktop\\testeICS.txt");
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		}
 	}
 }
