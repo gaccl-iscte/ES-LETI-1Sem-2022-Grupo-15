77fd2e6c2fb16ce93d0cff6c6cd2c9fb6c2f249a
bpaoo <bpaoo@iscte-iul.pt>
2022-11-17 15:26:55 +0000

    console to json

--- Projeto_ES/src/main/java/ES_2022_LETI_Grupo_15/Projeto_ES/toJSON.java
+++ Projeto_ES/src/main/java/ES_2022_LETI_Grupo_15/Projeto_ES/toJSON.java
@@ -1,11 +1,18 @@
 package ES_2022_LETI_Grupo_15.Projeto_ES;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.Scanner;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 public class toJSON {
 
@@ -15,6 +22,11 @@ public class toJSON {
 		SimpleDateFormat format = new SimpleDateFormat(" yyyyMMdd HHmmss");
 
 		Scanner scan = new Scanner (file);
+		JSONArray eventList = new JSONArray();
+		JSONArray testList = new JSONArray();
+		JSONArray examList = new JSONArray();
+
+		FileWriter fileJSON = new FileWriter("event.json");
 
 		while (scan.hasNextLine()) {
 
@@ -29,6 +41,7 @@ public class toJSON {
 
 			if(x.equals("Exame")){
 
+				JSONObject examDetails = new JSONObject();
 				String summary = next;
 				scan.next();
 				String dtstart = scan.nextLine();
@@ -36,13 +49,16 @@ public class toJSON {
 				scan.next();
 				String dtend = scan.nextLine();
 				dtend = dtend.trim();
-				
-				System.out.println("Summary: " + summary);
-				System.out.println("Início: " + dtstart);
-				System.out.println("Fim: " + dtend);
+				examDetails.put("Sumário", summary);
+				examDetails.put("Início", dtstart);
+				examDetails.put("Fim", dtend);
+				JSONObject examObject = new JSONObject(); 
+				examObject.put("Exame", examDetails);
+				examList.add(examObject);
 
 			} else if (x.equals("Teste") || x.equals("Avaliação")) {
 
+				JSONObject testDetails = new JSONObject();
 				String summary = next;
 				scan.next();
 				String dtstart = scan.nextLine();
@@ -50,24 +66,30 @@ public class toJSON {
 				scan.next();
 				String dtend = scan.nextLine();
 				dtend = dtend.trim();
-				
-				System.out.println("Summary: " + summary);
-				System.out.println("Início: " + dtstart);
-				System.out.println("Fim: " + dtend);
+				testDetails.put("Sumário", summary);
+				testDetails.put("Início", dtstart);
+				testDetails.put("Fim", dtend);
+				JSONObject testObject = new JSONObject(); 
+				testObject.put("Teste", testDetails);
+				testList.add(testObject);
 
 			} else {
 
+				JSONObject eventDetails = new JSONObject();
 				String summary = next;
 				scan.next();
 				String dtstart = scan.nextLine();
 				dtstart = dtstart.trim();
 				scan.next();
 				String dtend = scan.nextLine();
-				
-				System.out.println("Summary: " + summary);
-				System.out.println("Início: " + dtstart);
-				System.out.println("Fim: " + dtend);
-				
+				dtend = dtend.trim();
+				eventDetails.put("Sumário", summary);
+				eventDetails.put("Início", dtstart);
+				eventDetails.put("Fim", dtend);
+				JSONObject eventObject = new JSONObject(); 
+				eventObject.put("Evento", eventDetails);
+				eventList.add(eventObject);
+
 			}
 
 			scan.nextLine();
@@ -75,5 +97,14 @@ public class toJSON {
 		}
 		scan.close();
 
+		fileJSON.write(examList.toJSONString());
+		fileJSON.write(eventList.toJSONString());
+		fileJSON.write(testList.toJSONString());
+		fileJSON.flush();
+	}
+
+	public static void main(String[] args) throws IOException, ParseException {
+		File file = new File ("filtrado2.txt");
+		convert(file);
 	}
 }
