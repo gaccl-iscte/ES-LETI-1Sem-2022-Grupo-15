cf61131c8e5f6d79637f74b586704266681ef28b
tmmfo1 <tmmfo1@iscte-iul.pt>
2022-11-17 15:00:35 +0000

    URL to TXT changes

---
+++ Projeto_ES/src/main/java/ES_2022_LETI_Grupo_15/Projeto_ES/toTxt.java
@@ -0,0 +1,21 @@
+package ES_2022_LETI_Grupo_15.Projeto_ES;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+
+public class toTxt {
+	
+	static void convert(String urlStr, String file) throws IOException {
+        URL url = new URL(urlStr);
+        ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+        FileOutputStream fos = new FileOutputStream(file);
+        fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+        fos.close();
+        rbc.close();
+	}
+}
